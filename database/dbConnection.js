8d61ffd46c6989c71a141b295c18eb74a0b245ae
58Jamil58 <136060025+58Jamil58@users.noreply.github.com>
2024-07-01 17:37:28 +0530

    dbname

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "JOB_SEEKING",
+      dbName: "JobZee",
     })
     .then(() => {
       console.log("Connected to database.");
