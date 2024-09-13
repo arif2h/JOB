b93c430151909130da44bbfceb0e8e317adc77d1
arif_mohemmad <arifmohemmad612@gmail.com>
2024-09-13 20:49:22 +0530

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,7 +9,7 @@ export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("User Not Authorized", 401));
   }
   const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-
+ console.log("1");
   req.user = await User.findById(decoded.id);
 
   next();
